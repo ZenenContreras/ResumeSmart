bb4e7cee4bc0df917cc2ebb30b989199313403fb
Zenen Contreras Royero <zenencontreras1@gmail.com>
2025-10-09 00:22:12 -0500

    feat: testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Hero from '@/components/marketing/Hero';
 import HowItWorks from '@/components/marketing/HowItWorks';
 import PricingCards from '@/components/marketing/PricingCards';
 import Footer from '@/components/layout/Footer';
+import Testimonials from '@/components/marketing/Testimonials'
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-white overflow-hidden">
       <Header />
       <Hero />
+      <Testimonials />
       <HowItWorks/>
       <PricingCards/>
       <Footer />
